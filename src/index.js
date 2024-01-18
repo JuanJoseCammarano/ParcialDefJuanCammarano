512b8273c313c8c1dc66c07c6b0a40bfd7fc9187
JuanJoseCammarano <149250549+JuanJoseCammarano@users.noreply.github.com>
2024-01-18 18:32:37 +0000

    Instalación npm init, npm install y express morgan y ejs y creación del index.js

---
+++ src/index.js
@@ -0,0 +1,25 @@
+/*const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const WebSocket = require('ws');
+const path = require ('path');
+
+const wss = new WebSocket.Server({server:server});
+
+const port = process.env.PORT || 8080;
+
+app.get('/', function (req,res){
+    res.sendFile(path.join(__dirname, '/homepage.html'));
+});
+
+app.get('/admin', function(req,res){
+    res.sendFile(path.join(__dirname, '/admin.html'));
+});
+
+app.get('/index', function(req,res){
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(port);
+console.log('Server started at http://localhost:'+ port);*/
+console.log('Sexo');
