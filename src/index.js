0f6f7fdb3aa4e827fd48ee7c44f2c5eb921bddab
JuanJoseCammarano <149250549+JuanJoseCammarano@users.noreply.github.com>
2024-01-19 01:19:10 +0000

    Controlador websocket con páginas conectadas

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-/*const express = require('express');
+const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const WebSocket = require('ws');
@@ -7,19 +7,20 @@ const path = require ('path');
 const wss = new WebSocket.Server({server:server});
 
 const port = process.env.PORT || 8080;
-
+const pepe='sexo';
 app.get('/', function (req,res){
-    res.sendFile(path.join(__dirname, '/homepage.html'));
+    res.sendFile(path.join(__dirname, 'public/homepage.html'));
 });
 
 app.get('/admin', function(req,res){
-    res.sendFile(path.join(__dirname, '/admin.html'));
+    res.sendFile(path.join(__dirname, 'public/admin.html'));
 });
 
 app.get('/index', function(req,res){
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+
 app.listen(port);
-console.log('Server started at http://localhost:'+ port);*/
-console.log('Sexo');
+console.log('Server started at http://localhost:'+ port);
+
