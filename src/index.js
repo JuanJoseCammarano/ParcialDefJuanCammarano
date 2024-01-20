824c01ce74e95062c0f3c69381e77e86b455bdec
JuanJoseCammarano <149250549+JuanJoseCammarano@users.noreply.github.com>
2024-01-20 22:31:27 +0000

    Diferenciación entre usuario y admin en el menu y adelanto en el dibujo del hangman

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 8000
 const server = app.listen(port, () => console.log(`Listening on port ${port}`))
+let ids=[];
 //const WebSocket = require('ws');
 var anfitrion = '';
 const io = require('socket.io')(server, {
@@ -10,12 +11,36 @@ const io = require('socket.io')(server, {
         origin:'*',
     }
 });
+/*const filter = function(ids,element){
+    for(i=0;i<ids.length;i++){
+        if (ids[i]===element) delete ids[i];
+    }
+}*/
 io.on('connection', (socket) => {
-    console.log(socket.id);
+    ids.push(socket.id);
+    console.log(ids);
     socket.on('enviar-palabra-todos',(palabra,id)=>{
         console.log(palabra,id);
     })
+    socket.on('disconnect', () => {
+        console.log(socket.id);
+        for (i=0;i<ids.length;i++){
+            if (ids[i]===socket.id) ids.splice(i,1);
+        }
+        console.log(ids)
+        /*id=stringify(socket.id)
+        ids=ids.filter(id);
+        console.log(ids);*/
+        socket.emit('role')
+    });
+    socket.emit('usersIds', ids)
+    socket.emit('role')
 });
+/*io.on('disconnection', (socket) =>{
+    console.log("Recibido");
+    ids.filter(socket);
+    console.log(ids);
+})*/
 //const io = require("socket.io")(server);
 //const socket = io.connect("http://localhost:8080", { forceNew: true });
 //const wss = new WebSocket.Server({server: server});
--- src/public/scripts/homepage.js
+++ src/public/scripts/homepage.js
@@ -1,6 +1,33 @@
-const socket = io('http://localhost:8000');
+socket = io('http://localhost:8000');
+let idsUsers = [];
+let userId = socket.id;
+//const { response } = require("express");
+const hostPanel= document.querySelector(".anfitrion");
 socket.on('connect', () => {
-    console.log('someone connected!');
+    /*socket.on('userId', (id)=>{
+        userId=id
+        console.log(userId);
+    })*/
+    console.log(socket.id);
     let palabra='pepe';
-    socket.emit('enviar-palabra-todos',palabra, socket.id)
+    socket.on('usersIds', (ids)=>{
+        idsUsers=[...ids];
+    })
+    let word="pepe";
+    socket.on('role', () => {
+        if (idsUsers.length===1){
+            const h2 = document.createElement("h2");
+            h2.innerText = word;
+            hostPanel.appendChild(h2);
+        }
+    })
+    socket.emit('enviar-palabra-todos',palabra, socket.id);
 });
+/*
+let palabra='';
+async function fetchWord() {
+    const response = await fetch('https://pow-3bae6d63ret5.deno.dev/word')
+    const word = await response.json();
+    palabra=JSON.stringify(word).subString(7,JSON.stringify(word).length -1);
+    console.log(palabra);
+}*/
