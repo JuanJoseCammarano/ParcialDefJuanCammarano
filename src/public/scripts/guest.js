a17a899a758e2905430c0ecdabd2dcff639fa7b5
JuanJoseCammarano <149250549+JuanJoseCammarano@users.noreply.github.com>
2024-01-20 01:25:08 +0000

    Casi listo juego individual falta error y acierto y dibujo

--- src/public/scripts/guest.js
+++ src/public/scripts/guest.js
@@ -1,26 +1,40 @@
 //const { arrayBuffer } = require("stream/consumers");
-//const keyboardDiv = document.querySelector(".keyboard");
+const keyboardDiv = document.querySelector(".keyboard");
 let word = "PAW";
-/*for (let i= 97; i <= 122; i++){
-        const button = document.createElement("button");
-        button.innerText = String.fromCharCode(i);
-        keyboardDiv.appendChild(button);
-}*/
-//const underScores = (word) => {
-    let underScores ='';
-    const guess = document.querySelector('p');
-    for (let i=0; i<word.length; i++){
-    underScores+="_    "
+String.prototype.replaceAt = function(index, replacement) {
+    if (index >= this.length) {
+        return this.valueOf();
     }
-    guess.innerText = underScores;
+ 
+    return this.substring(0, index) + replacement + this.substring(index + 1);
+}
+let underScores ='';
+const guess = document.querySelector('p');
+for (let i=0; i<word.length; i++){
+underScores+="_"
+}
+guess.innerText = underScores;
+for (let i= 97; i <= 122; i++){
+        const button = document.createElement("button");
+        button.innerText = String.fromCharCode(i).toUpperCase();
+        keyboardDiv.appendChild(button);
+        button.addEventListener("click", function () {
+            const letter = button.innerHTML;
+            if (word.includes(letter) && !guess.innerHTML.includes(letter)){
+                for (let i = 0; i < word.length; i++){
+                    if (word[i] === letter){
+                        /*stringAnterior=guess.innerText.substring(0,i);
+                        stringRestante=guess.innerText.substring(i+1,word.length-1);
+                        guess.innerHTML = stringAnterior + letter + stringRestante;*/
+                        guess.innerText = guess.innerText.replaceAt(i,letter);
+                    }
+                }  
+            } 
+        })
+}
+//const underScores = (word) => {
     //const palabra = document.createElement('p')
     /*const p = document.createElement("p");
     p.innerText = underScores;
     wordDiv.appendChild(p);*/
 //}
-/*
-function handleGuess(str){
-    for (let i=0; i<word.length; i++){
-        if ('A' === word[i]) console.log ("Esta");
-    }
-}*/
